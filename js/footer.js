60ee348d4b370a59c24fdba7177ad80349288b76
夏目贵志 <6@9i3.cn>
2018-02-22 16:58:22 +0800

    gx

--- js/footer.js
+++ js/footer.js
@@ -4,4 +4,4 @@ document.writeln("        </div>");
 document.writeln("    </div>");
 document.writeln("    <script src=\'https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js\'></script>");
 document.writeln("    <script src=\'https://cdn.bootcss.com/amazeui/2.7.2/js/amazeui.min.js\'></script>");
-document.writeln("<script type=\'text/javascript\' src=\'js\/\/duoshuo.js\'></script>");
+document.writeln("<script type=\'text/javascript\' src=\'../js\/\/duoshuo.js\'></script>");
