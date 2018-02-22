c1cf46b25a97923cb38eb00c09a081bbea9fa11c
夏目贵志 <6@9i3.cn>
2018-02-22 14:27:32 +0800

    blog

---
+++ js/data.js
@@ -0,0 +1,38 @@
+var transform = {'tag':'li','html':'${link}'};  
+  
+var data = [  
+      {'link':'<a href="/post/1.html" target="_blank" class="am-list-item-hd ">htaccess伪静态实例分享</a>'},
+      {'link':'<a href="/post/2.html" target="_blank" class="am-list-item-hd ">六十一新开始！</a>'},  
+      {'link':''}, 	  
+      {'link':''}	  
+];  
+  
+document.getElementById('jl').innerHTML = json2html.transform(data,transform);  
+
+var transform = {'tag':'li','html':'${link}'};  
+  
+var data = [  
+      {'link':'<a href="https://pan.baidu.com/s/1miIppVU" target="_blank" class="am-list-item-hd "> [清水ユウ]甘美男子</a>'}, 	 	  
+      {'link':''}	  
+];  
+  
+document.getElementById('js').innerHTML = json2html.transform(data,transform);  
+
+var transform = {'tag':'li','html':'${link}'};  
+  
+var data = [  
+      {'link':'<a href="https://pan.baidu.com/s/1snbpwQx" target="_blank" class="am-list-item-hd ">【在许墨怀里】分享-各类耽美文合集</a>'}, 
+      {'link':''}, 		  
+      {'link':''}	  
+];  
+  
+document.getElementById('wz').innerHTML = json2html.transform(data,transform);  
+
+var transform = {'tag':'li','html':'${link}'};  
+  
+var data = [  
+      {'link':''}, 	  
+      {'link':''}	  
+];  
+  
+document.getElementById('ty').innerHTML = json2html.transform(data,transform);  
